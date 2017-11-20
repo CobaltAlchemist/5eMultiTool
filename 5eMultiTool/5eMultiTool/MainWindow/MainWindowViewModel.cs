14aba07a8d30474f9a452709215a03018b064efd
Corey Senger <coreysenger@gmail.com>
2017-11-20 12:24:14 -0600

    started working on stuff

--- 5eMultiTool/5eMultiTool/MainWindow/MainWindowViewModel.cs
+++ 5eMultiTool/5eMultiTool/MainWindow/MainWindowViewModel.cs
@@ -39,6 +39,41 @@ namespace _5eMultiTool
             JavaScriptSerializer js = new JavaScriptSerializer();
             JsonClasses.RootMonsterObject monsters = js.Deserialize<JsonClasses.RootMonsterObject>(json);
             MessageBox.Show(monsters.monster[0].ToString());
+            _5eDatabaseDataContext db = new _5eDatabaseDataContext();
+            foreach (JsonClasses.JsonMonster jm in monsters.monster)
+            {
+                Monster m = new Monster();
+                m.Name = jm.name;
+                m.Type = jm.type;
+                m.Source = jm.source;
+                m.Alignment = jm.alignment;
+                m.AC = Convert.ToByte(jm.ac);
+                m.AvgHP = Convert.ToInt16(jm.hp.Substring(0, jm.hp.IndexOf(' ')));
+                m.HP = jm.hp.Substring(jm.hp.IndexOf(" ") + 1);
+                m.Speed = jm.speed;
+                m.Strength = Convert.ToByte(jm.strength);
+                m.Dexterity = Convert.ToByte(jm.dexterity);
+                m.Constitution = Convert.ToByte(jm.constitution);
+                m.Intelligence = Convert.ToByte(jm.intelligence);
+                m.Wisdom = Convert.ToByte(jm.wisdom);
+                m.Charisma = Convert.ToByte(jm.charisma);
+                m.Passive_Perception = Convert.ToByte(jm.passivePerception);
+                if (jm.cr.Contains("/"))
+                {
+                    m.CR = Convert.ToDecimal(jm.cr[0]) / Convert.ToDecimal(jm.cr[2]);
+                }
+                else
+                    m.CR = Convert.ToDecimal(jm.cr);
+                db.Monsters.InsertOnSubmit(m);
+                List<Trait> ts = new List<Trait>();
+                foreach (JsonClasses.JsonLatr jl in jm.trait)
+                {
+                    Trait t = new Trait();
+                    t.Name = jl.name;
+                    t.Description = jl.text;
+                }
+                db.SubmitChanges();
+            }
             return;
         }
     }
--- 5eMultiTool/5eMultiTool/Properties/Settings.Designer.cs
+++ 5eMultiTool/5eMultiTool/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace _5eMultiTool.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "14.0.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.3.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -31,6 +31,17 @@ namespace _5eMultiTool.Properties {
         public string _5eDatabaseConnectionString {
             get {
                 return ((string)(this["_5eDatabaseConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=(LocalDB)\\ProjectsV13;AttachDbFilename=|DataDirectory|\\5eDatabase.mdf" +
+            ";Integrated Security=True")]
+        public string _5eDatabaseConnectionString1 {
+            get {
+                return ((string)(this["_5eDatabaseConnectionString1"]));
             }
         }
     }
