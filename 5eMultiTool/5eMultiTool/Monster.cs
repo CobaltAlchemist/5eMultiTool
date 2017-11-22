bab718ba65fe62aa2ca06d9993163e5db7d11aec
Corey Lawrence Senger <coreysenger@gmail.com>
2017-11-22 13:45:20 -0600

    redoin' shiz

---
+++ 5eMultiTool/5eMultiTool/Monster.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5eMultiTool
+{
+    class Monster : ObservableObject
+    {
+        string _name;
+        public string Name { get {return _name; }
+            set { _name = value; OnPropertyChanged("Name"); }
+        CharSize _size;
+        public string Size
+        {
+            get { return _size; }
+            set { _size = value; OnPropertyChanged("Size"); }
+        }
+}
